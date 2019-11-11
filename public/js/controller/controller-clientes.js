240055890f1fc249292acaa714cdfc3d55fd2437
Jeremias ramirez <jereramirez499@gmail.com>
2019-11-10 22:36:04 -0400

    trayendo datos del server

--- public/js/controller/controller-clientes.js
+++ public/js/controller/controller-clientes.js
@@ -1,4 +1,11 @@
-appLoader.controller("clientes.controller", ["$scope", function($scope){
+appLoader.controller("clientes.controller", ["$scope", "$http", function($scope, $http){
+    $scope.paises = [];
+    $http({
+        method: "GET",
+        url: "php/controller/controller-countries.php"
+    }).then((res)=>{
+        $scope.paises = res.data.clients;
 
+    })
 
 }]);
