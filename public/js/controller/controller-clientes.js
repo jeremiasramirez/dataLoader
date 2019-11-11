4fc5e7b20c79eccf5afce94ad13813beb743c68f
Jeremias ramirez <jereramirez499@gmail.com>
2019-11-11 11:38:44 -0400

    agregando logica de pagination

--- public/js/controller/controller-clientes.js
+++ public/js/controller/controller-clientes.js
@@ -1,11 +1,49 @@
 appLoader.controller("clientes.controller", ["$scope", "$http", function($scope, $http){
+
+
+    // request
     $scope.paises = [];
+
+    $scope.sizeDataPage = null;
+    $scope.lenPage = 0;
     $http({
         method: "GET",
         url: "php/controller/controller-countries.php"
     }).then((res)=>{
         $scope.paises = res.data.clients;
+        $scope.sizeDataPage = res.data.clients.length;
+        $scope.lenPage = Math.ceil(res.data.clients.length / 6);
+    });
 
-    })
+
+    /*pagination page size*/
+    $scope.positionPage = 6;
+
+    /*visualization page*/
+    $scope.positionPageView = 1;
+
+
+    //page right click
+
+    $scope.pageRight = function(){
+        if($scope.sizeDataPage){
+            $scope.positionPage += 6;
+            $scope.positionPageView += 1;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }]);
