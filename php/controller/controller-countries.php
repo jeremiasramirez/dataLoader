6909931605af55cd6eb54090b93763273e5cc7f0
Jeremias ramirez <jereramirez499@gmail.com>
2019-11-10 22:35:37 -0400

    mejorando backend

--- php/controller/controller-countries.php
+++ php/controller/controller-countries.php
@@ -2,10 +2,7 @@
 
 include "../model/clients.php";
 
-$get_clients = $_GET["getclients"];
-
-if(isset($get_clients) && $get_clients == "get"){
     $clients = new clients();
     $clients->get_clients();
-}
+
 
